55f91a3c5cf06880cbbf1d6e407417cff17b1438
luiz <luizmarcelolm@hotmail.com>
2023-03-09 20:10:31 -0300

    números aleatórios

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('gerador');
+
+
+//evento de click, pegando valores dos inputs
+button.addEventListener('click', function(){
+    const quantidade = parseInt(document.getElementById('quantidade').value)
+    const minimo = parseInt(document.getElementById('minimo').value)
+    const maximo = parseInt(document.getElementById('maximo').value)
+
+    //solução encontrada devido quando é gerada a lista o número máximo digitado pelo usuário nunca aparece
+    var maximo1 = maximo +1;
+    var numeros=[];
+
+    // fórmula para gerar quantidade de números entre mínimo e máximo 
+    while(numeros.length < quantidade){
+        var aleatorio = Math.floor(Math.random()*(maximo1 - minimo)+minimo);
+        if(numeros.indexOf(aleatorio) == -1)
+        numeros.push(aleatorio);
+    }
+            //mostra na tela no span1 a lista de números sorteados
+            document.querySelector('#sorteio > span1').textContent = numeros;
+
+            //para achar o segundo múmero maior da lista usei o comando "sort" para colocar em ordem crescente a lista
+             var crescente = numeros.sort();
+
+            //utilizei essa função para pegar o penultimo números da lista crescente que corresponde ao segundo maior número da lista
+             var segundoMaior = crescente[crescente.length-2];
+
+             //mostra na tela o segundo maior número
+            document.querySelector('#segundoMaior > span2').textContent = segundoMaior;
+    
+
+    
+});
