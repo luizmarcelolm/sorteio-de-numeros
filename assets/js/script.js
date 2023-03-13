dfb14bee0a5bd6a25d532f13dba612f32262460a
luiz <luizmarcelolm@hotmail.com>
2023-03-13 18:25:17 -0300

    modificação da formula

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,88 +4,47 @@ var maximo = document.getElementById('maximo')
 var quantidade = document.getElementById('quantidade')
 var numeros=[];
 
+
         //função que cria números aleatórios
         const gerarNumeros = () => {
+            
             while(numeros.length < parseInt(quantidade.value)){
                 var aleatorio = Math.floor(Math.random()*(parseInt(maximo.value) - parseInt(minimo.value)) + parseInt(minimo.value));
                 if(numeros.indexOf(aleatorio) == -1)
                 numeros.push(aleatorio);
-            }
-            document.querySelector('#sorteio > span1').textContent = numeros;
+            } 
+            document.querySelector('#sorteio > span1').textContent = "Números criados " + numeros;
             acharSegundoMaior();
+            novaLista();
         } 
+
+        function novaLista() {   
+            document.getElementById("gerar").style.display = "none";           
+        }
 
         //função de acha o segundo maior número da lista
         const acharSegundoMaior = () =>{
-        var maior = numeros[0]; 
-         
-
+        var maior = numeros[0];
+        var segundoMaior = 0; 
+             
                 for (i = 1; i < numeros.length; i++) {                  
                     var atual = numeros[i];
-                    if( maior > atual)
-                       maior = maior;
-                       auxiliar = atual;
-                    if( maior < atual)
-                       maior = atual;
+
+                    if (maior > atual && segundoMaior < atual){
+                        maior = maior; 
+                        segundoMaior = atual;
+                    }
+
+                    if (maior > atual && segundoMaior > atual){
+                        segundoMaior = segundoMaior;
+                        maior  = maior;      
+                    }
+
+                    if (maior < atual && segundoMaior < atual){
+                        segundoMaior = maior;
+                        maior = atual;       
+                    }
                 }        
-                    document.querySelector('#segundoMaior > span2').textContent = maior;
+                document.querySelector('#segundoMaior > span2').textContent = "Segundo maior número da lista é " + segundoMaior;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- const acharSegundoMaior = () =>{
-        var maior = numeros[0];
-
-                for ( i = 1; i < numeros.length; i++) {
-                    var posicao = numeros[i];
-                    if( posicao > maior)
-                    maior = posicao;
-                }
-                    document.querySelector('#segundoMaior > span2').textContent = maior;
-        }
-
-
-//evento de click, pegando valores dos inputs
-const button = document.getElementById('gerador');
-button.addEventListener('click', function(){
-    const quantidade = parseInt(document.getElementById('quantidade').value)
-    const minimo = parseInt(document.getElementById('minimo').value)
-    const maximo = parseInt(document.getElementById('maximo').value)
-
-    //solução encontrada devido quando é gerada a lista o número máximo digitado pelo usuário nunca aparece
-    var maximo1 = maximo +1;
-    var numeros=[];
-
-    // fórmula para gerar quantidade de números entre mínimo e máximo 
-    while(numeros.length < quantidade){
-        var aleatorio = Math.floor(Math.random()*(maximo1 - minimo)+minimo);
-        if(numeros.indexOf(aleatorio) == -1)
-        numeros.push(aleatorio);
-    }
-            //mostra na tela no span1 a lista de números sorteados
-            document.querySelector('#sorteio > span1').textContent = numeros;
-
-            //para achar o segundo múmero maior da lista usei o comando "sort" para colocar em ordem crescente a lista
-             var crescente = numeros.sort();
-
-            //utilizei essa função para pegar o penultimo números da lista crescente que corresponde ao segundo maior número da lista
-             var segundoMaior = crescente[crescente.length-2];
-
-             //mostra na tela o segundo maior número
-            document.querySelector('#segundoMaior > span2').textContent = segundoMaior;
-    
-});
-
-*/
 
